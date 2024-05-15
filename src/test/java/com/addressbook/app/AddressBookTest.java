92142b8c11e1b26bb701f7e63302543c687d5fd2
Julian Humble <humblejulian@gmail.com>
2024-05-15 09:39:13 +0100

    AB-US1-T1-passed

--- src/test/java/com/addressbook/app/AddressBookTest.java
+++ src/test/java/com/addressbook/app/AddressBookTest.java
@@ -1,4 +1,38 @@
 package com.addressbook.app;
 
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+
 public class AddressBookTest {
+
+    @Nested
+    @DisplayName("Address Book Tests")
+    class AddressBookTests {
+
+        private AddressBook testAddressBook;
+
+        @BeforeEach
+        public void setUp() {
+            testAddressBook = new AddressBook();
+
+        }
+
+        @AfterEach
+        public void tearDown() {
+            testAddressBook = null;
+        }
+
+        @Test
+        @DisplayName("Using addContact increases length of contactList by 1")
+        void testContactListIncreasesBy1() {
+            // Arrange
+            int expected = testAddressBook.getContactList().size() + 1;
+            Contact testContact = mock(Contact.class);
+            // Act
+            testAddressBook.addContact(testContact);
+            // Assert
+            assertEquals(testAddressBook.getContactList().size(), expected);
+        }
+    }
 }
