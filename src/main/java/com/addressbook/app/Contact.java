34051aa4b263e5a7985004de08ee37aa9d72d3d7
Julian Humble <humblejulian@gmail.com>
2024-05-15 17:56:39 +0100

    C-US2-T1-passed

--- src/main/java/com/addressbook/app/Contact.java
+++ src/main/java/com/addressbook/app/Contact.java
@@ -1,4 +1,14 @@
 package com.addressbook.app;
 
 public class Contact {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public Contact(String name) {
+        this.name = name;
+    }
 }
---
+++ src/test/java/com/addressbook/app/ContactTest.java
@@ -0,0 +1,33 @@
+package com.addressbook.app;
+
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.condition.DisabledIfSystemProperties;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ContactTest {
+
+    @Nested
+    @DisplayName("Contact Tests")
+    class ContactTests{
+
+        private Contact testContact;
+
+        @Nested
+        @DisplayName("US-2: I want my contact to have a name record")
+        class US2ContactTests{
+
+            @Test
+            @DisplayName("Test that passing a name into the Contact constructor results in the same name being assigned")
+            void testCorrectNameIsAssigned() {
+                // Arrange
+                String testName = "test2";
+                // Act
+                Contact testContact = new Contact(testName);
+                // Assert
+                assertEquals(testContact.getName(), testName);
+            }
+
+        }
+    }
+}
