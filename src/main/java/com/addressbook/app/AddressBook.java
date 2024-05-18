3895001627f8818b1d53036290ff5c53e1c2119a
Julian Humble <humblejulian@gmail.com>
2024-05-18 22:20:22 +0100

    REFACTOR: AB-US5-T1-passed

--- src/main/java/com/addressbook/app/AddressBook.java
+++ src/main/java/com/addressbook/app/AddressBook.java
@@ -42,4 +42,14 @@ public class AddressBook {
         return searchResults;
     }
 
+    public static String displayContactsToUser(ArrayList<Contact> listOfContacts) {
+        String stringToReturn = "";
+        for (int i = 0; i < listOfContacts.size(); i++) {
+            stringToReturn += ("Contact " + (i+1) + ":\n\n");
+            stringToReturn += listOfContacts.get(i).displayContact();
+            stringToReturn += "\n\n";
+        }
+        return stringToReturn;
+    }
+
 }
--- src/test/java/com/addressbook/app/AddressBookTest.java
+++ src/test/java/com/addressbook/app/AddressBookTest.java
@@ -1,5 +1,6 @@
 package com.addressbook.app;
 
+import com.addressbook.app.utils.ResultsPrinter;
 import org.junit.jupiter.api.*;
 
 import java.util.ArrayList;
@@ -151,6 +152,34 @@ public class AddressBookTest {
                 ArrayList<Contact> actual = testAddressBook.findSearchResults("Test");
                 // Assert
                 assertEquals(expected, actual);
+            }
+
+        }
+
+        @Nested
+        @DisplayName("US-5: I want to be able to search my address book by name and see the results")
+        class NewUS5AddressBookTests{
+
+            @Test
+            @DisplayName("Test that displayContactsToUser() returns the desired string")
+            void testDisplayContactsToUserReturnsDesiredString() {
+                // Arrange
+                Contact testContact1 = mock(Contact.class), testContact2 = mock(Contact.class);
+                when(testContact1.displayContact()).thenReturn("Name: Test1 Test1\nPhone: 07123456789\nEmail: test@test.test");
+                when(testContact2.displayContact()).thenReturn("Name: Test2 Test2\nPhone: 07123456789\nEmail: test@test.test");
+
+                ArrayList<Contact> testArrayList = new ArrayList<>(Arrays.asList(testContact1, testContact2));
+
+                String expectedString = "Contact 1:\n\n"+
+                        "Name: Test1 Test1\nPhone: 07123456789\nEmail: test@test.test"+
+                        "\n\nContact 2:\n\n"+
+                        "Name: Test2 Test2\nPhone: 07123456789\nEmail: test@test.test"
+                        + "\n\n";
+
+                // Act
+                String returnedString = testAddressBook.displayContactsToUser(testArrayList);
+                // Assert
+                assertEquals(expectedString, returnedString);
             }
 
         }
