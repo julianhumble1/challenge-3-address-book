9167758ab3608e25e1afc421004d7638f426fafb
Julian Humble <humblejulian@gmail.com>
2024-05-19 16:50:18 +0100

    IV-US13-T2-passed

---
+++ src/test/java/com/addressbook/app/UserInputMenuTest.java
@@ -0,0 +1,49 @@
+package com.addressbook.app;
+
+import com.addressbook.app.utils.UserInputMenu;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayInputStream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class UserInputMenuTest {
+
+    @Nested
+    @DisplayName("User Input Menu Tests")
+    class UserInputMenuTests {
+
+        @Nested
+        @DisplayName("US13: I want to be able to trigger adding a contact")
+        class US13UIMTests{
+
+            @Test
+            @DisplayName("takeUserNumberChoice returns the input when input is valid")
+            void takeUserNumberChoiceReturnsInputWhenInputIsValid() {
+                // Arrange
+                String input = "3\n";
+                ByteArrayInputStream in = new ByteArrayInputStream(input.getBytes());
+                System.setIn(in);
+                // Act
+                int result = UserInputMenu.takeUserNumberChoice(1, 5);
+                // Assert
+                assertEquals(3, result);
+            }
+
+            @Test
+            @DisplayName("takeUserNumberChoice returns second input when first input is invalid but second is valid")
+            void takeUserNumberChoiceReturnsSecondInputWhenFirstInputIsInvalidButSecondIsValid() {
+                // Arrange
+                String input = "10\n3\n";
+                ByteArrayInputStream in = new ByteArrayInputStream(input.getBytes());
+                System.setIn(in);
+                // Act
+                int result = UserInputMenu.takeUserNumberChoice(1, 5);
+                // Assert
+                assertEquals(3, result);
+            }
+        }
+    }
+}
